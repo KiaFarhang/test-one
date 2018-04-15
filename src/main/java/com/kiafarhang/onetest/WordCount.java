94e753423b30ded2acaf930126abf3be8cd1e3a9
Kia Farhang <kfarhang0@gmail.com>
2018-04-15 09:46:16 -0500

    Add code to count the occurrences of words in a string
    I chose a Map structure to represent the word counts in this project. The bulk of this commit contains a static WordCount class method to generate Map<String, Integer> from a provided string.
    
    The method strips punctuation from the string, then splits it on white space. For each non-empty string in the resulting split array, the method either adds it to the map with a value of 1 (for one occurrence) or adds one to the value if it already exists in the map. The commit contains tests for edge cases such as capitalization, words with apostrophes, and white space separating words.
    
    Finally, this commit also adds a constructor for the WordCount class itself. All the constructor does is take a given string and store it as a word count map, which can be reached with a public getter method.
    
    I decided to split the word count creation into its own static method rather than putting it all in the constructor for a few reasons:
    
    1) Should we want to change how we create the word count maps (i.e. maybe we DO want to treat capitalization as a different word), we'd just have to cook up a new method and change one line in the class constructor to use it instead.
    
    2) I've found it's a bit more cumbersome to test constructor logic, as each unit test has to concern itself with calling the constructor and then asserting using getter methods.

--- src/main/java/com/kiafarhang/onetest/WordCount.java
+++ src/main/java/com/kiafarhang/onetest/WordCount.java
@@ -1,7 +1,53 @@
 package com.kiafarhang.onetest;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.HashMap;
+
 public class WordCount {
+    private Map<String, Integer> wordCount;
+
+    public WordCount(String string) {
+        this.wordCount = createWordCountMap(string);
+    }
+
+    public Map<String, Integer> getWordCount() {
+        return this.wordCount;
+    }
+
     public static String stripPunctuation(String string) {
+        // We keep apostrophes and hyphens, as they can form words
         return string.replaceAll("[\\p{Punct}&&[^'-]]", "");
+    }
+
+    public static Map<String, Integer> createWordCountMap(String string) {
+        Map<String, Integer> wordCountMap = new HashMap<String, Integer>();
+
+        String punctuationStripped = stripPunctuation(string);
+
+        String[] splitOnWhiteSpace = punctuationStripped.split("\\s");
+
+        for (String word : splitOnWhiteSpace) {
+            // Normalize the word
+            word = word.trim().toLowerCase();
+
+            // At this point, we're still counting the empty string "" as a word. So we only move further
+            // if the word contains at least one character.
+
+            if (word.length() > 0) {
+                // This will be null if the word was absent
+                Integer wordsCurrentValue = wordCountMap.putIfAbsent(word, 1);
+
+                // So if it's NOT null, it equals whatever the current count for the given word is, and we should update it.
+                if (wordsCurrentValue != null) {
+                    wordCountMap.replace(word, wordsCurrentValue + 1);
+                }
+
+            }
+
+        }
+
+        return wordCountMap;
+
     }
 }
--- src/test/java/com/kiafarhang/onetest/WordCountTest.java
+++ src/test/java/com/kiafarhang/onetest/WordCountTest.java
@@ -5,6 +5,8 @@ import com.kiafarhang.onetest.WordCount;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.Map;
+
 public class WordCountTest {
     @Test
     public void stripsPunctuationFromStrings() {
@@ -25,5 +27,91 @@ public class WordCountTest {
         String original = "We don't want to lose hyphens in words like well-oiled, either.";
         String expected = "We don't want to lose hyphens in words like well-oiled either";
         assertEquals(expected, WordCount.stripPunctuation(original));
+    }
+
+    @Test
+    public void createsOneWordMap() {
+        String string = "A";
+        Map<String, Integer> wordCount = WordCount.createWordCountMap(string);
+        assertEquals(1, wordCount.size(), 0);
+        assertTrue(wordCount.containsKey("a"));
+        assertEquals(1, wordCount.get("a"), 0);
+    }
+
+    @Test
+    public void createsMultiWordMap() {
+        String string = "Apple banana coconut";
+        Map<String, Integer> wordCount = WordCount.createWordCountMap(string);
+        assertEquals(3, wordCount.size(), 0);
+
+        assertTrue(wordCount.containsKey("apple"));
+        assertEquals(1, wordCount.get("apple"), 0);
+
+        assertTrue(wordCount.containsKey("banana"));
+        assertEquals(1, wordCount.get("banana"), 0);
+
+        assertTrue(wordCount.containsKey("coconut"));
+        assertEquals(1, wordCount.get("coconut"), 0);
+    }
+
+    @Test
+    public void createsMultiWordMapWithDuplicateWords() {
+        String string = "Apple banana coconut apple";
+        Map<String, Integer> wordCount = WordCount.createWordCountMap(string);
+        assertEquals(3, wordCount.size(), 0);
+
+        assertTrue(wordCount.containsKey("apple"));
+        assertEquals(2, wordCount.get("apple"), 0);
+
+        assertTrue(wordCount.containsKey("banana"));
+        assertEquals(1, wordCount.get("banana"), 0);
+
+        assertTrue(wordCount.containsKey("coconut"));
+        assertEquals(1, wordCount.get("coconut"), 0);
+    }
+
+    @Test
+    public void handlesPunctuation() {
+        String string = "Apple! Banana coconut apple?";
+        Map<String, Integer> wordCount = WordCount.createWordCountMap(string);
+        assertEquals(3, wordCount.size(), 0);
+
+        assertTrue(wordCount.containsKey("apple"));
+        assertEquals(2, wordCount.get("apple"), 0);
+
+        assertTrue(wordCount.containsKey("banana"));
+        assertEquals(1, wordCount.get("banana"), 0);
+
+        assertTrue(wordCount.containsKey("coconut"));
+        assertEquals(1, wordCount.get("coconut"), 0);
+    }
+
+    @Test
+    public void respectsApostrophesAndHyphens() {
+        String string = "Don't, isn't, well-formed.";
+        Map<String, Integer> wordCount = WordCount.createWordCountMap(string);
+        assertEquals(3, wordCount.size(), 0);
+
+        assertTrue(wordCount.containsKey("don't"));
+        assertEquals(1, wordCount.get("don't"), 0);
+
+        assertTrue(wordCount.containsKey("isn't"));
+        assertEquals(1, wordCount.get("isn't"), 0);
+
+        assertTrue(wordCount.containsKey("well-formed"));
+        assertEquals(1, wordCount.get("well-formed"), 0);
+    }
+
+    @Test
+    public void handlesWhitespace() {
+        String string = "Hello!\n     Hello World.";
+        Map<String, Integer> wordCount = WordCount.createWordCountMap(string);
+        assertEquals(2, wordCount.size(), 0);
+
+        assertTrue(wordCount.containsKey("hello"));
+        assertEquals(2, wordCount.get("hello"), 0);
+
+        assertTrue(wordCount.containsKey("world"));
+        assertEquals(1, wordCount.get("world"), 0);
     }
 }
