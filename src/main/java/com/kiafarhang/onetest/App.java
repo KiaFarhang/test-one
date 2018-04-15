b403a2310ecf393d0e74a5765c43ab3c1767be7f
Kia Farhang <kfarhang0@gmail.com>
2018-04-15 14:44:42 -0500

    Complete application to build word count histogram from input paragraph
    This commit adds a writeHistogramToFile method to the top-level App class. Given String parameters for input and output paths, the method builds a word count for the input file, sorts it by highest occurring words and passes each word and occurrence count to the Histogram function to generate a histogram line. The method then writes that master String of histogram lines to a file it creates (or overwrites, if it already exists) at the given output path.
    
    This commit includes a test for that method, which uses a separate text file in the test directory and asserts the result of running the method on it and creating a new file in that same directory.
    
    The commit modifies the App class's main method to call the writeHistogramToFile method. Should a user prefer, this commit has updated the Gradle configuration to let them pass custom input and output file parameters. Without them, the program defaults to reading from 'input.txt' and writing to 'output.txt', both in the project's root directory.
    
    Finally, the commit git ignores the output results for both the test case and the live main method.

--- src/main/java/com/kiafarhang/onetest/App.java
+++ src/main/java/com/kiafarhang/onetest/App.java
@@ -1,11 +1,54 @@
 package com.kiafarhang.onetest;
 
+import com.kiafarhang.onetest.WordCount;
+import com.kiafarhang.onetest.Histogram;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
+    public static void main(String[] args) {
+        // In case someone runs the default gradle 'run' task without passing arguments,
+        // we use the default 'input.txt' and 'output.txt' files for the program.
+
+        // However, we have the ability to customize the files we read from and write to if we so choose.
+        if (args.length < 2) {
+            writeHistogramToFile("input.txt", "output.txt");
+        } else {
+            writeHistogramToFile(args[0], args[1]);
+        }
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    public static void writeHistogramToFile(String inputPath, String outputPath) {
+        try {
+            String input = new String(Files.readAllBytes(Paths.get(inputPath)), StandardCharsets.UTF_8);
+            Map<String, Integer> wordCount = new WordCount(input).getWordCount();
+            Map<String, Integer> sortedWordCount = WordCount.sortMapByIntegerValueHighestFirst(wordCount);
+
+            List<Map.Entry<String, Integer>> wordList = new ArrayList<Map.Entry<String, Integer>>(
+                    sortedWordCount.entrySet());
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (Map.Entry<String, Integer> entry : wordList) {
+                stringBuilder.append(Histogram.generateHistogramLine(entry.getKey(), entry.getValue()) + "\n");
+            }
+
+            String output = stringBuilder.toString();
+
+            try {
+                Files.write(Paths.get(outputPath), output.getBytes(StandardCharsets.UTF_8));
+            } catch (IOException exception) {
+                throw new Error(exception.toString());
+            }
+
+        } catch (IOException exception) {
+            throw new Error(exception.toString());
+        }
     }
 }
--- src/test/java/com/kiafarhang/onetest/AppTest.java
+++ src/test/java/com/kiafarhang/onetest/AppTest.java
@@ -1,12 +1,34 @@
 package com.kiafarhang.onetest;
 
+import com.kiafarhang.onetest.App;
+
 import org.junit.Test;
+import org.junit.Before;
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.List;
+import java.util.ArrayList;
 
 public class AppTest {
     @Test
-    public void testAppHasAGreeting() {
-        App classUnderTest = new App();
-        assertNotNull("app should have a greeting", classUnderTest.getGreeting());
+    public void writesToFileProperly() throws IOException {
+
+        String inputPath = "src/test/java/com/kiafarhang/onetest/input-test.txt";
+        String outputPath = "src/test/java/com/kiafarhang/onetest/output-test.txt";
+
+        App.writeHistogramToFile(inputPath, outputPath);
+
+        String output = new String(Files.readAllBytes(Paths.get(outputPath)), StandardCharsets.UTF_8);
+
+        String expected = "apple | ==== (4)\nbanana | == (2)\n";
+
+        assertEquals(expected, output);
+
     }
+
 }
